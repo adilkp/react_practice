b6b456c85db786dd455c125494cb383759da486e
adilkp <adilkpat@gmail.com>
2024-01-05 22:46:14 +0530

    react functional components

--- App.js
+++ App.js
@@ -1,10 +1,35 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client';
 
-const heading = React.createElement(
-  "h1",
-  { id: "heading", xyz: "abc" },
-  "Hello World from React..."
+const heading = React.createElement("h1", {id: "heading"}, "Adil Khan !");
+
+console.log(heading)
+
+// React element
+// const jsxHeading = (
+//   <h1 className='head' tabIndex='5'>
+//     Welcome to React 🚀
+//   </h1>
+// );
+
+const Title = () => (
+  <h1 className='head' tabIndex='5'>
+    Welcome to React using JSX
+  </h1>
+)
+
+// console.log(jsxHeading)
+
+// React functional component
+//Component composition
+
+const HeadingComponent = () => (
+  <div id='container'>
+    <Title />
+    <h1 className='heading'>Welcome to React Functional component🚀</h1>
+  </div>
 );
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+
+const root = ReactDOM.createRoot(document.getElementById("root"))
+
+root.render(<HeadingComponent />)
