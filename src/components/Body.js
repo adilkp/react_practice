088ac8f76ed24499dd877b60490068e8b31254f6
adilkp <adilkpat@gmail.com>
2024-01-18 12:34:02 +0530

    correcting the API call

--- src/components/Body.js
+++ src/components/Body.js
@@ -14,13 +14,13 @@ const Body = () => {
     const fetchData = async () => {
       const data = await fetch(
         "https://www.swiggy.com/dapi/restaurants/list/v5?lat=13.0138405&lng=77.5876668&is-seo-homepage-enabled=true&page_type=DESKTOP_WEB_LISTING"
-        );
-
+      );
+        
       const json = await data.json();
 
-      console.log(json.data.cards[5].card.card.gridElements.infoWithStyle.restaurants)
+      console.log(json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants)
 
-      setListofRestaurants(json.data.cards[5].card.card.gridElements.infoWithStyle.restaurants)
+      setListofRestaurants(json.data.cards[4].card.card.gridElements.infoWithStyle.restaurants)
       };
 
     return (
